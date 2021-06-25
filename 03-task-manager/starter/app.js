37dbd510280ecf549da446158379b23234fd9f02
Graham Allan <grahamallan31+git@gmail.com>
2021-06-25 15:15:50 +0100

    new code

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+require('dotenv').config()
+const connectDB = require('./db/connect')
+const express = require('express')
+const tasks = require('./routes/tasks')
+
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/v1/tasks',tasks)
+
+app.get('/hello', (request, responce) => {
+    responce.status(200).send("Task Manager App")
+})
+
+const port = 3000
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server listining on port ${port}...`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
